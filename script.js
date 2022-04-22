d31177847df8ae17f2ab0e817b8e6c2dc7b06698
Fatima Sultana <fatima.shanaz30@gmail.com>
2022-04-22 12:50:15 +0530

    Progress Bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio');
+const progressContainer=document.getElementById('progress-container');
+const progress=document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -74,8 +76,20 @@ function nextSong(){
     loadSong(songs[songIndex]);  
     playSong();  
 }
+
+//Progress Bar
+function progressBar(e){
+    if(isPlaying){
+        const { duration , currentTime }=e.srcElement;
+        //update progress bar
+        const progressPercent=(currentTime/duration)*100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 //On Load select first song
 loadSong(songs[songIndex]);
 
 prevBtn.addEventListener('click',prevSong);
 nextBtn.addEventListener('click',nextSong);
+music.addEventListener('timeupdate',progressBar);
